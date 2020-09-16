34af7d4b3bebff2d878541de950c17746f8f3b8a
Carlos Moreno <omorenodovale@gmail.com>
2020-09-16 15:12:23 -0300

    Expanding test coverage

--- app/tests/test_calculator.py
+++ app/tests/test_calculator.py
@@ -59,3 +59,66 @@ def test_sub_232_less_190():
     payload = {"number_one": "11101000", "number_two": "10111110"}
     response = client.post("/sub/", json=payload)
     assert response.json() == {"result": "00101010"}
+
+
+def test_mult_1_by_1():
+    """Must return 1 in binary"""
+    payload = {"number_one": "00000001", "number_two": "00000001"}
+    response = client.post("/mult/", json=payload)
+    assert response.json() == {"result": "00000001"}
+
+
+def test_mult_2_by_1():
+    """Must return 1 in binary"""
+    payload = {"number_one": "00000010", "number_two": "00000001"}
+    response = client.post("/mult/", json=payload)
+    assert response.json() == {"result": "00000010"}
+
+
+def test_mult_40_by_3():
+    """Must return 120 in binary"""
+    payload = {"number_one": "00101000", "number_two": "00000011"}
+    response = client.post("/mult/", json=payload)
+    assert response.json() == {"result": "01111000"}
+
+
+def test_div_2_by_1():
+    """Must return 2 in binary"""
+    payload = {"number_one": "00000010", "number_two": "00000001"}
+    response = client.post("/div/", json=payload)
+    assert response.json() == {"result": "00000010"}
+
+
+def test_div_180_by_3():
+    """Must return 60 in binary"""
+    payload = {"number_one": "10110100", "number_two": "00000011"}
+    response = client.post("/div/", json=payload)
+    assert response.json() == {"result": "00111100"}
+
+
+def test_div_12_by_4():
+    """Must return 3 in binary"""
+    payload = {"number_one": "00001100", "number_two": "00000100"}
+    response = client.post("/div/", json=payload)
+    assert response.json() == {"result": "00000011"}
+
+
+def test_mod_2_by_1():
+    """Must return 0 in binary"""
+    payload = {"number_one": "00000010", "number_two": "00000001"}
+    response = client.post("/mod/", json=payload)
+    assert response.json() == {"result": "00000000"}
+
+
+def test_mod_10_by_4():
+    """Must return 2 in binary"""
+    payload = {"number_one": "00001010", "number_two": "00000100"}
+    response = client.post("/mod/", json=payload)
+    assert response.json() == {"result": "00000010"}
+
+
+def test_mod_100_by_5():
+    """Must return 0 in binary"""
+    payload = {"number_one": "01100100", "number_two": "00000101"}
+    response = client.post("/mod/", json=payload)
+    assert response.json() == {"result": "00000000"}
