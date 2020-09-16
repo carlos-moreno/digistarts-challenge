e67a90f6e338d2682c639ad2942ff14120b403a4
Carlos Moreno <omorenodovale@gmail.com>
2020-09-16 09:34:24 -0300

    Adding docstring in the endpoints

--- app/routers/calculator.py
+++ app/routers/calculator.py
@@ -20,29 +20,37 @@ dict_of_operations = {
 
 
 def format_return(number: str):
+    """Returns the number formatted in binary
+    :param number: number to be formatted
+    """
     return {"result": number[2:].zfill(8)}
 
 
 @router.post("/sum/", tags=["calculator"])
 def binary_sum(item: Item):
+    """Returns the sum of two binary numbers"""
     return format_return(dict_of_operations["sum"](item))
 
 
 @router.post("/sub/", tags=["calculator"])
 def binary_sub(item: Item):
+    """Returns the subtraction of two binary numbers"""
     return format_return(dict_of_operations["sub"](item))
 
 
 @router.post("/mult/", tags=["calculator"])
 def binary_mult(item: Item):
+    """Returns the multiplication of two binary numbers"""
     return format_return(dict_of_operations["mult"](item))
 
 
 @router.post("/div/", tags=["calculator"])
 def binary_div(item: Item):
+    """Returns the division of two binary numbers"""
     return format_return(dict_of_operations["div"](item))
 
 
 @router.post("/mod/", tags=["calculator"])
 def binary_mod(item: Item):
+    """Returns the remainder of the division of two binary numbers"""
     return format_return(dict_of_operations["mod"](item))
